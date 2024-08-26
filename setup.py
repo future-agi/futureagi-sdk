934752f63fa4f0f84b0c8ea794d38b4669699da1
gsapra <sapra.garvit33@gmail.com>
2024-08-26 14:19:59 +0530

    readme updated

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ def get_requirements(path: str):
     return [l.strip() for l in open(path)]
 
 setup(
-    name='fi-tcs',
+    name='futureagi',
     version='0.1.1',
-    author='FutureAgi',
+    author='Future AGI',
     author_email='noreply@mail.futureagi.com',
-    description='We help GenAI teams maintain high-accuracy for their Models in production.',
-    long_description=open('README.md').read(),
+    description='Empowering GenAI teams to maintain peak model accuracy in production environments.',
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/future-agi/clients',
+    url='https://github.com/future-agi/client',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
